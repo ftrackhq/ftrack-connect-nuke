b02d38ae63ada4edb1317d75fabaf31b013a27c6
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2015-02-09 12:41:52 +0100

    change to ftrack types for assigend tasks (not in use atm)

--- source/ftrack_connect_nuke/millftrack_nuke/ui/generic/base_dialog.py
+++ source/ftrack_connect_nuke/millftrack_nuke/ui/generic/base_dialog.py
@@ -462,7 +462,10 @@ class BaseIODialog(BaseDialog):
 
     warning = None
     if self.current_task != None:
-      self._not_my_task = self._user.getId() not in self.current_task.users_ids
+      user_tasks = [t.getId() for t in self._user.getTasks()]
+
+      self._not_my_task = self.current_task.getId() not in user_tasks
+
       if self._not_my_task:
         warning = "This task is not assigned to you. You might need to ask your \
 supervisor to assign you to this task before publishing any asset.\n\
