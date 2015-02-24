78e68ea5f531ee0fdfffe8e704f09edc8c20f5cf
Carl Claesson <carl.claesson@ftrack.com>
2015-02-24 17:19:43 +0100

    Added notification call to action hooks.

--- resource/hook/ftrack_connect_nuke_hook.py
+++ resource/hook/ftrack_connect_nuke_hook.py
@@ -249,6 +249,14 @@ class ApplicationLauncher(ftrack_connect.application.ApplicationLauncher):
             self.plugin_path, 'FOUNDRY_ASSET_PLUGIN_PATH', environment
         )
 
+        # Set the FTRACK_EVENT_PLUGIN_PATH to include the notification callback
+        # hooks.
+        environment = ftrack_connect.application.appendPath(
+            os.path.join(
+                self.plugin_path, 'crew_hook'
+            ), 'FTRACK_EVENT_PLUGIN_PATH', environment
+        )
+
         return environment
 
 
