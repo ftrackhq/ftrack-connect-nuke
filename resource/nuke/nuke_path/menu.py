a41acc9db661e52272fc1b263f12f36622ca0f10
langeli <lorenzo.angeli@efestolab.uk>
2015-02-25 13:47:47 +0100

    try except for bootstrapping the plugin

--- resource/nuke/nuke_path/menu.py
+++ resource/nuke/nuke_path/menu.py
@@ -1,5 +1,11 @@
 # Import to bootstrap foundry api.
 import ftrack_connect_nuke
 import ftrack_connect
-import foundry.assetmgr
+try:
+	# included in ftrack-connect-foundry
+	import assetmgr_nuke
+except:
+	# part of nuke
+	import foundry.assetmgr
+
 import nuke
