f43f3bf20b9cb77adef30f9a36a1f944b0640efd
Mattias Lagergren <mattias.lagergren@ftrack.com>
2015-03-18 14:04:07 +0100

    Bumped version to 0.1.2

--- setup.py
+++ setup.py
@@ -118,8 +118,8 @@ setup(
     dependency_links=[
         ('https://bitbucket.org/ftrack/lowdown/get/0.1.0.zip'
          '#egg=lowdown-0.1.0'),
-        ('https://bitbucket.org/ftrack/ftrack-connect/get/0.1.7.zip'
-        '#egg=ftrack-connect-0.1.7'),
+        ('https://bitbucket.org/ftrack/ftrack-connect/get/0.1.9.zip'
+        '#egg=ftrack-connect-0.1.9'),
         ('https://bitbucket.org/ftrack/ftrack-connect-foundry/get/master.zip'
         '#egg=ftrack-connect-foundry-0.1.0')
     ],
