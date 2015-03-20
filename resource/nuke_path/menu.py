284f3a693eca0387dd5b1195d3975b3c6d18ab8d
Mattias Lagergren <mattias.lagergren@ftrack.com>
2015-03-20 17:39:56 +0100

    Removed import of assetmgr since it crashes nuke studio

--- resource/nuke_path/menu.py
+++ resource/nuke_path/menu.py
@@ -4,11 +4,11 @@
 # Import to bootstrap foundry api.
 import ftrack_connect_nuke
 import ftrack_connect
-try:
-    # part of nuke
-    import foundry.assetmgr
-except:
-    # included in ftrack-connect-foundry
-    import assetmgr_nuke
+# try:
+#     # part of nuke
+#     import foundry.assetmgr
+# except:
+#     # included in ftrack-connect-foundry
+#     import assetmgr_nuke
 
 import nuke
