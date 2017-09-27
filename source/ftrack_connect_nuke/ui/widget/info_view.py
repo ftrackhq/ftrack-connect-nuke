27c20eb5be0cad78c0fb5c680479581be8e7c439
lorenzo angeli <lorenzo.angeli@efestolab.uk>
2017-09-27 10:27:22 +0200

    rework widget to prepare for user warning on selection.

--- source/ftrack_connect_nuke/ui/widget/info_view.py
+++ source/ftrack_connect_nuke/ui/widget/info_view.py
@@ -1,6 +1,7 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2015 ftrack
 
+from QtExt import QtWidgets
 from ftrack_connect_foundry.ui.info_view import InfoView
 import nuke
 
@@ -13,25 +14,35 @@ class AssetInfoView(
     _kIdentifier = 'com.ftrack.asset_information_panel'
     _kDisplayName = 'Asset Info'
 
+    def render_info(self):
+        '''Build and layout widget.'''
+        layout = self.layout()
+        label = QtWidgets.QLabel(
+            '<h1><b>Please select an asset node.</b></h1>'
+        )
+        layout.addWidget(label)
+
     def __init__(self, bridge, parent=None):
         '''Initialise InvfoView.'''
+        super(AssetInfoView, self).__init__(
+            bridge, parent=parent
+        )
+
         nodes = nuke.selectedNodes()
 
-        if not nodes:
+        if not nodes or not nodes[0]:
+            self.render_info()
             return
 
         node = nodes[0]
 
-        if not node:
-            return
-
         has_knob = node.knob('assetVersionId')
-        if not has_knob:
+        if has_knob:
+
+            asset_id = has_knob.value()
+            asset_id = asset_id.split('ftrack://')[-1].split('?')[0]
+            self.setEntityReference(asset_id)
+
+        else:
+            self.render_info()
             return
-
-        asset_id = has_knob.value()
-        asset_id = asset_id.split('ftrack://')[-1].split('?')[0]
-
-        super(AssetInfoView, self).__init__(
-            bridge, parent=parent, entityReference=asset_id
-        )
