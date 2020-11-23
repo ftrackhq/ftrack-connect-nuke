4f1792bbbae4b81ffdec6761362038076155afb2
lorenzo <lorenzo.angeli@ftrack.com>
2020-11-23 14:32:23 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ from setuptools import setup, find_packages, Command
 from pkg_resources import parse_version
 import pip
 
-if parse_version(pip.__version__) < parse_version('19.3.0'):
-    raise ValueError('Pip should be version 19.3.0 or higher')
-
 from pip._internal import main as pip_main
 
 # Define paths
@@ -109,7 +106,7 @@ class BuildPlugin(Command):
         )
 
         # Install local dependencies
-        pip_main.main(
+        pip_main(
             [
                 'install',
                 '.',
