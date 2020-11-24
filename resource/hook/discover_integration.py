deafb4b46748a7b1eb82cae44fead58d9f0d6501
lorenzo <lorenzo.angeli@ftrack.com>
2020-11-24 15:28:18 +0100

    update hook

--- resource/hook/discover_integration.py
+++ resource/hook/discover_integration.py
@@ -33,20 +33,20 @@ def on_discover_nuke_integration(session, event):
     data = {
         'integration': {
             'name': 'ftrack-connect-nuke',
-            'version': integration_version
-        },
-        'env': {
-            'PYTHONPATH.prepend': sources,
-            'NUKE_PATH': nuke_connect_plugins,
-            'FOUNDRY_ASSET_PLUGIN_PATH': ftrack_connect_nuke_resource_path,
-            'QT_PREFERRED_BINDING':  os.pathsep.join(['PySide2', 'PySide']),
-            'NUKE_USE_FNASSETAPI': '1',
-            'FTRACK_TASKID.set': task['id'],
-            'FTRACK_SHOTID.set': task['parent']['id'],
-            'LOGNAME.set': session._api_user,
-            'FTRACK_APIKEY.set': session._api_key,
-            'FS.set': task['parent']['custom_attributes'].get('fstart', '1.0'),
-            'FE.set': task['parent']['custom_attributes'].get('fend', '100.0')
+            'version': integration_version,
+            'env': {
+                'PYTHONPATH.prepend': sources,
+                'NUKE_PATH': nuke_connect_plugins,
+                'FOUNDRY_ASSET_PLUGIN_PATH': ftrack_connect_nuke_resource_path,
+                'QT_PREFERRED_BINDING':  os.pathsep.join(['PySide2', 'PySide']),
+                'NUKE_USE_FNASSETAPI': '1',
+                'FTRACK_TASKID.set': task['id'],
+                'FTRACK_SHOTID.set': task['parent']['id'],
+                'LOGNAME.set': session._api_user,
+                'FTRACK_APIKEY.set': session._api_key,
+                'FS.set': task['parent']['custom_attributes'].get('fstart', '1.0'),
+                'FE.set': task['parent']['custom_attributes'].get('fend', '100.0')
+            }
         }
     }
     return data
