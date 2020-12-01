ebfae2cae37bea997137f166424fa4b107ee741b
lorenzo <lorenzo.angeli@ftrack.com>
2020-12-01 08:56:39 +0100

    cleanup hook

--- resource/hook/discover_integration.py
+++ resource/hook/discover_integration.py
@@ -1,25 +1,18 @@
 # :coding: utf-8
 # :copyright: Copyright (c) 2015 ftrack
 
-import getpass
 import sys
-import pprint
-import logging
-import re
 import os
 import functools
-import traceback
-
 import ftrack_api
-import ftrack_connect.application
 
 
 def on_discover_nuke_integration(session, event):
     cwd = os.path.dirname(__file__)
     sources = os.path.abspath(os.path.join(cwd, '..', 'dependencies'))
-    ftrack_connect_nuke_resource_path = os.path.abspath(os.path.join(
+    ftrack_connect_nuke_resource_path = os.path.join(
         cwd, '..',  'resource')
-    )
+
     sys.path.append(sources)
 
     from ftrack_connect_nuke import __version__ as integration_version
@@ -52,18 +45,16 @@ def on_discover_nuke_integration(session, event):
     return data
 
 
-
-
 def register(session):
     '''Subscribe to application launch events on *registry*.'''
     if not isinstance(session, ftrack_api.session.Session):
         return
 
-
     handle_event = functools.partial(
         on_discover_nuke_integration,
         session
     )
+
     session.event_hub.subscribe(
         'topic=ftrack.connect.application.launch'
         ' and data.application.identifier=nuke_*',
